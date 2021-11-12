aa0886e61770a011c0beec84245ba8ca02635003
Oskari-Juvakka <oskari.juvakka@gmail.com>
2021-11-12 17:47:37 +0200

    pylint error test

--- src/varasto.py
+++ src/varasto.py
@@ -31,7 +31,7 @@ class Varasto:
             self.saldo = 0.0
 
             return kaikki_mita_voidaan
-
+        print("12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890")
         self.saldo = self.saldo - maara
         return maara
 
